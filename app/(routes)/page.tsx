07e8ca338019468018599bd972ade4788f69f73f
Natha Wiguna <nathawiguna1@gmail.com>
2023-12-10 07:04:15 +0800

    first commit

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,7 +8,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
   const products = await getProducts({ isFeatured: true });
-  const billboard = await getBillboard("0e12e5cf-29ab-4529-b8d5-c5371dae1f7b");
+  const billboard = await getBillboard("a5c5c8f0-9e98-4b94-af66-0f3d6aabcc76");
 
   return (
     <Container>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -6,14 +6,14 @@ import NavbarActions from "@/components/navbar-actions";
 import getCategories from "@/actions/get-categories";
 
 const Navbar = async () => {
-  const categories = await getCategories();
+  const categories = await getCategories()
 
   return ( 
     <div className="border-b">
       <Container>
         <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center">
-          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
-            <p className="font-bold text-xl">STORE</p>
+          <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">        
+            <p className="font-bold text-xl">DANIEL FURQON PROPERTY</p>
           </Link>
           <MainNav data={categories} />
           <NavbarActions />
